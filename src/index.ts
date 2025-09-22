3c5da3f15c77134556b9ca40f1152b9a97edc2ec
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-09-22 19:05:09 +0300

    создается app с левой боковой панелью

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
+import App from "./view/app";
+import LeftPanel from "./view/components/left-panel/left_panel";
 import "./index.scss";
+
 document.addEventListener("DOMContentLoaded", function () {
 	try {
-		const root: HTMLDivElement | null = document.querySelector("#root");
-		const app: HTMLDivElement | null = document.createElement("div");
-
-		if (!root || !app) {
-			throw new Error("Не удалось загрузить #root или .app");
-		}
-		app.classList.add("app");
-		app.textContent = "Hello, Todo-list!";
-		root?.append(app);
+		const left_panel = new LeftPanel().getComponent();
+		new App(left_panel, document.createElement("div")).render();
 	} catch (error) {
 		console.log(error);
 	}
