f766a0fdb0fb27cc97355ea832850ab7c9a2373c
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-09-22 20:38:01 +0300

    добавлен объект класса MainContent

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import App from "./view/app";
 import LeftPanel from "./view/components/left-panel/left_panel";
+import MainContent from "./view/components/mainContent/main_content";
 import "./index.scss";
 
 document.addEventListener("DOMContentLoaded", function () {
 	try {
-		const left_panel = new LeftPanel().getComponent();
-		new App(left_panel, document.createElement("div")).render();
+		const leftPanel = new LeftPanel().getComponent();
+		const mainContent = new MainContent().getContent();
+		new App(leftPanel, mainContent).render();
 	} catch (error) {
 		console.log(error);
 	}
